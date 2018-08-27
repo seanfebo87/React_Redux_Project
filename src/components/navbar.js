b25ae21831191eaf0d62f3173848d78fe6a66a8a
Sean Febo <35676649+seanfebo87@users.noreply.github.com>
2018-08-27 13:44:23 -0700

    added navabar function with css from bulma.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,2 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav class="navbar is-fixed-top">
+      <div class="container">
+        <div id="navbarMenu" class="navbar-menu">
+          <div class="navbar-end">
+            <Link class="navbar-item" to="/">
+              home
+            </Link>
+            <Link class="navbar-item" to="/about">
+              about
+            </Link>
+            <Link class="navbar-item" to="/recipes">
+              recipes
+            </Link>
+            <Link class="navbar-item" to="/recipe/new">
+              add recipe
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
