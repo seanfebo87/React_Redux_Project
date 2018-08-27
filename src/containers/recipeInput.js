1b0599be9789e860c2c911c8e16670f7f0806eea
Sean Febo <35676649+seanfebo87@users.noreply.github.com>
2018-08-27 14:26:05 -0700

    started class. defined state.

--- src/containers/recipeInput.js
+++ src/containers/recipeInput.js
@@ -4,6 +4,14 @@ import { bindActionCreators } from "redux";
 import * as recipeActions from "../actions/recipeActions";
 import recipeForm from "../components/recipeForm";
 
+class RecipeInput extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      recipe: Object.assign({}, props.recipe),
+    };
+  }
+
 function mapStateToProps(state, ownProps) {
   let recipe = { name: "", instructions: "", ingredients: "", picture: "" };
 
