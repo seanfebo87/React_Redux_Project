d710645de86a6ee2b5fc197bb44f223a24437452
Sean Febo <35676649+seanfebo87@users.noreply.github.com>
2018-08-27 13:39:44 -0700

    created app.js with imports needed.

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import navbar from "../components/navbar";
+import home from "../components/home";
+import about from "../components/about";
+import recipePage from "../components/recipePage";
+import recipesPage from "../containers/recipesPage";
+import recipeInput from "../containers/recipeInput";
