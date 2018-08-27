c760d9785b795d62c3e6df7eb750591d5ba55a2f
Sean Febo <35676649+seanfebo87@users.noreply.github.com>
2018-08-27 13:12:16 -0700

    added function to create store.

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,3 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import mainReducer from "../reducers/index";
+
+export default function maketore(initialState) {
+  return createStore(mainReducer, initialState, applyMiddleware(thunk));
+}
