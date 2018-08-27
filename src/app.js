96dcefe8ae5cdd7bae777ce5d53b9718700b9d6a
Sean Febo <35676649+seanfebo87@users.noreply.github.com>
2018-08-27 13:46:22 -0700

    fixed paths.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import navbar from "../components/navbar";
-import home from "../components/home";
-import about from "../components/about";
-import recipePage from "../components/recipePage";
-import recipesPage from "../containers/recipesPage";
-import recipeInput from "../containers/recipeInput";
+import navbar from "./components/navbar";
+import home from "./components/home";
+import about from "./components/about";
+import recipePage from "./components/recipePage";
+import recipesPage from "./containers/recipesPage";
+import recipeInput from "./containers/recipeInput";
 
 class App extends Component {
   render() {
