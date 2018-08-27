1e80f262c203d6828c236cc75a169a1c6b808d30
Sean Febo <35676649+seanfebo87@users.noreply.github.com>
2018-08-27 14:33:44 -0700

    added render function and placed values for recipeForm component.

--- src/containers/recipeInput.js
+++ src/containers/recipeInput.js
@@ -27,6 +27,21 @@ class RecipeInput extends Component {
       this.props.history.push('/recipes');
     }
 
+  render(){
+      return (
+        <section class="hero is-light is-fullheight is-bold">
+          <div class="hero-body">
+            <recipeForm type="text" name="name" value={this.state.recipe.name} placeholder="Name" onChange={this.handleOnChange} />
+            <recipeForm type="text" name="ingredients" value={this.state.recipe.ingredients} placeholder="Ingredients" onChange={this.handleOnChange} />
+            <recipeForm type="text" name="instructions" value={this.state.recipe.instructions} placeholder="Instructions" onChange={this.handleOnChange} />
+            <recipeForm type="text" name="picture" value={this.state.recipe.picture} placeholder="Picture" onChange={this.handleOnChange} />
+            <input type="submit" onSubmit={this.handleOnSubmit} />
+          </div>
+        </section>
+      )
+    }
+  }
+
 function mapStateToProps(state, ownProps) {
   let recipe = { name: "", instructions: "", ingredients: "", picture: "" };
 
