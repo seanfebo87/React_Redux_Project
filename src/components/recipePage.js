4c378db19f03cdf972c16d27f12a43a52279d403
Sean Febo <35676649+seanfebo87@users.noreply.github.com>
2018-08-27 14:01:32 -0700

    added recipePage function for layout.

--- src/components/recipePage.js
+++ src/components/recipePage.js
@@ -1,5 +1,16 @@
 import React from "react";
 
+const recipePage = ({ recipe }) => {
+  return (
+    <section class="hero is-light is-fullheight is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <h3>{recipe.instructions}</h3>
+        </div>
+      </div>
+    </section>
+  );
+};
 const mapStateToProps = (state, ownProps) => {
   const recipe = state.recipes.find(
     recipe => String(recipe.id) === ownProps.match.params.id
