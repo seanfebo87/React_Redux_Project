6b77f36a03ece16ba10f7f14d34280644ebff1bc
Sean Febo <35676649+seanfebo87@users.noreply.github.com>
2018-08-27 14:14:38 -0700

    rendered small card box for each recipe.

--- src/components/recipeCard.js
+++ src/components/recipeCard.js
@@ -1,2 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+const recipeCard = ({ recipe }) => {
+  return (
+    <Link class="box" to={"/recipe/" + recipe.id}>
+      <article class="media">
+        <div class="media-left">
+          <figure class="image is-64x64">
+            <img src={recipe.picture} />
+          </figure>
+        </div>
+        <div class="media-content">
+          <div class="content">
+            <p>
+              <strong>{recipe.name}</strong>
+              <br />
+              {recipe.ingredients}
+            </p>
+          </div>
+        </div>
+      </article>
+    </Link>
+  );
+};
+
+export default recipeCard;
