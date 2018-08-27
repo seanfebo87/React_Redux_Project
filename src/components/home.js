e44965b86e80235725743b7415238198975d966e
Sean Febo <35676649+seanfebo87@users.noreply.github.com>
2018-08-27 13:50:22 -0700

    created home function that renders layout.

--- src/components/home.js
+++ src/components/home.js
@@ -1,1 +1,18 @@
 import React from "react";
+
+const home = () => {
+  return (
+    <section class="hero is-light is-fullheight is-bold">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">
+            cookbook...<br />
+            ...a place for scrumptious delights.
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default home;
