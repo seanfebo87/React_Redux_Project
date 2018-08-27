b2f0080ac8272fe2a4387db08db29c2bf4a8b032
Sean Febo <35676649+seanfebo87@users.noreply.github.com>
2018-08-27 13:55:34 -0700

    about function that renders layout.

--- src/components/about.js
+++ src/components/about.js
@@ -1,1 +1,19 @@
 import React from "react";
+
+const about = () => {
+  return (
+    <section class="hero is-light is-fullheight is-bold">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h3>
+            This simple app is used to post your favorite recipes. Users can
+            post the name of the recipe, ingredients, instructions, and a
+            picture.
+          </h3>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default about;
