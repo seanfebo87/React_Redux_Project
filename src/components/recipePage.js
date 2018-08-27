ce9207d3a183609477dbdd36609db8d66948703e
Sean Febo <35676649+seanfebo87@users.noreply.github.com>
2018-08-27 14:00:26 -0700

    added mapStateToProps to find specific recipe.

--- src/components/recipePage.js
+++ src/components/recipePage.js
@@ -1,1 +1,12 @@
 import React from "react";
+
+const mapStateToProps = (state, ownProps) => {
+  const recipe = state.recipes.find(
+    recipe => String(recipe.id) === ownProps.match.params.id
+  );
+  if (recipe) {
+    return { recipe: recipe };
+  } else {
+    return { recipe: {} };
+  }
+};
