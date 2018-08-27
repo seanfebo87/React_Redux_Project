c7fce22f9d36883accc6e08dfd2077b26c18104f
Sean Febo <35676649+seanfebo87@users.noreply.github.com>
2018-08-27 13:28:34 -0700

    added fetch function to perform get request to rails api

--- src/actions/recipeActions.js
+++ src/actions/recipeActions.js
@@ -1,1 +1,18 @@
 import * as types from "./actionTypes";
+
+function loadRecipes(recipes) {
+  return { type: types.LOAD_RECIPE, recipes };
+}
+
+export function loadRecipe() {
+  return function(dispatch) {
+    return fetch("/recipes")
+      .then(response => response.json())
+      .then(recipes => {
+        dispatch(loadRecipes(recipes));
+      })
+      .catch(error => {
+        throw error;
+      });
+  };
+}
