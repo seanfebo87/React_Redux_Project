ea6b05e151428850cff4554d5788c98b7beecb60
Sean Febo <35676649+seanfebo87@users.noreply.github.com>
2018-08-27 13:06:39 -0700

    added render and const store to index.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,12 @@ import App from "./app";
 import { Provider } from "react-redux";
 import makeStore from "./stores/store";
 import "bulma/css/bulma.css";
+
+const store = makeStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
