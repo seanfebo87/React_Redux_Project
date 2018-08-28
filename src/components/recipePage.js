e9bc72b4363cf4266e9cf51bd1e10d1e4d8cb66a
Sean Febo <35676649+seanfebo87@users.noreply.github.com>
2018-08-27 22:24:34 -0700

    changed view of recipe page.

--- src/components/recipePage.js
+++ src/components/recipePage.js
@@ -2,16 +2,37 @@ import React from "react";
 import { connect } from "react-redux";
 
 const RecipePage = ({ recipe }) => {
-  return (
-    <section class="hero is-light is-fullheight is-bold">
+    return (
+      <section class="hero is-light is-fullheight is-bold">
+      <div class="hero-head">
+          <br />
       <div class="hero-body">
-        <div class="container">
-          <h3>{recipe.instructions}</h3>
+        <div class="box">
+        <article class="media">
+        <div class="media-left">
+          <figure class="image is-64x64">
+            <img src={recipe.picture} />
+          </figure>
         </div>
+        <div class="media-content">
+          <div class="content">
+            <p>
+              <strong>{recipe.name}</strong>
+              <br />
+              {recipe.ingredients}
+              <br />
+              {recipe.instructions}
+            </p>
+          </div>
+        </div>
+      </article>
       </div>
-    </section>
+      </div>
+      </div>
+      </section> 
   );
 };
+
 const mapStateToProps = (state, ownProps) => {
   const recipe = state.recipes.find(
     recipe => String(recipe.id) === ownProps.match.params.id
