d6801ae2ded3381aefbece64808f6c35d10b756f
Sean Febo <35676649+seanfebo87@users.noreply.github.com>
2018-08-29 22:05:22 -0700

    added vote button and counter to recipeCard page.

--- src/components/recipeCard.js
+++ src/components/recipeCard.js
@@ -1,26 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const RecipeCard = ({ recipe }) => {
+class RecipeCard extends React.Component {
+  constructor(props) {
+  super(props)
+  this.state = {
+    votes: this.props.recipe.votes
+  }
+}
+  
+  voteRecipe = (id) => {
+  this.setState({votes: this.props.recipe.votes += 1})
+  id = this.props.recipe.id
+    return fetch(`/recipes/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: id
+    })
+  };
+
   return (
-    <Link class="box" to={"/recipe/" + recipe.id}>
+    <div>
+    <Link class="box" to={"/recipe/" + this.props.recipe.id}>
       <article class="media">
         <div class="media-left">
           <figure class="image is-64x64">
-            <img src={recipe.picture} />
+            <img src={this.props.recipe.picture} />
           </figure>
         </div>
         <div class="media-content">
           <div class="content">
             <p>
-              <strong>{recipe.name}</strong>
+              <strong>{this.props.recipe.name}</strong>
               <br />
-              {recipe.ingredients}
+              {this.props.recipe.ingredients}
             </p>
           </div>
         </div>
       </article>
     </Link>
+    <button onClick={this.voteRecipe}>VOTE</button>
+    <p>{this.state.votes}</p>
+    </div>
   );
 };
 
