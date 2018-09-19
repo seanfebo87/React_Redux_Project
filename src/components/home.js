66f310fad2a0083d9003b2b7599da58df4eefd07
Sean Febo <seanfebo@techie.com>
2018-09-19 15:03:17 -0700

    changed puncutation

--- src/components/home.js
+++ src/components/home.js
@@ -7,7 +7,7 @@ const Home = () => {
         <div class="container has-text-centered">
           <h1 class="title">
             cookbook...<br />
-            ...a place for scrumptious delights.
+            ...a place for scrumptious delights!
           </h1>
         </div>
       </div>
