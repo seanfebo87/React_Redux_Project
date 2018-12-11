09e4cabb79f141c9175d0c084dac1546847522c9
Sean Febo <35676649+seanfebo87@users.noreply.github.com>
2018-12-10 22:38:18 -0800

    changed about statement.

--- src/components/about.js
+++ src/components/about.js
@@ -7,8 +7,7 @@ const About = () => {
         <div class="container has-text-centered">
           <h3>
             This simple app is used to post your favorite recipes. Users can
-            post the name of the recipe, ingredients, instructions, and a
-            picture.
+            post a name, a picture, and the link to the recipe.
           </h3>
         </div>
       </div>
