7bc7e42a6a76088664b33b3d9b195b82afc060d5
Sean Febo <35676649+seanfebo87@users.noreply.github.com>
2018-12-30 12:15:43 -0800

    removed sorting function.

--- src/actions/recipeActions.js
+++ src/actions/recipeActions.js
@@ -4,29 +4,12 @@ function loadRecipes(recipes) {
   return { type: types.LOAD_RECIPE, recipes };
 }
 
-function sortRecipes(recipes) {
-  return { type: types.SORT_RECIPE, recipes };
-}
-
 export function loadRecipe() {
   return function(dispatch) {
     return fetch("https://cookbookrailsapi.herokuapp.com/recipes")
       .then(response => response.json())
       .then(recipes => {
         dispatch(loadRecipes(recipes));
-      })
-      .catch(error => {
-        throw error;
-      });
-  };
-}
-
-export function sortRecipe() {
-  return function(dispatch) {
-    return fetch("https://cookbookrailsapi.herokuapp.com/recipes")
-      .then(response => response.json())
-      .then(recipes => {
-        dispatch(sortRecipes(recipes));
       })
       .catch(error => {
         throw error;
